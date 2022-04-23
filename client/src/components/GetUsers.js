421b91bcedf950776813dce92d53697274be0e98
hkava <hkava@tvo.org>
2022-04-22 21:29:18 -0600

    accordion functionality and styling for traffic control

--- client/src/components/GetUsers.js
+++ client/src/components/GetUsers.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { GlobalState, DatabaseRequest } from '../utils/GlobalContext';
 import DeleteConfirmation from './DeleteConfirmation';
+import Chevron from '../images/chevron.svg';
 
 function GetUsers() {
 
     const globalState = useContext(GlobalState);
     const databaseContext = useContext(DatabaseRequest);  
+
+    const [selected, setSelected] = useState(null);
 
     const [courseNames, setCourseNames] = useState([]);
 
@@ -35,6 +38,14 @@ function GetUsers() {
         }
     });
 
+    const toggle = (index) => {
+        if (selected === index) {
+            return setSelected(null);
+        }
+
+        setSelected(index);
+    };
+
     const handleModal = (e, data) => {
         if (e.key === 'Enter' || e.code === 'Space' || e.type === 'click') {
             globalState.updateState({ showModal: true, modalData: data })
@@ -51,29 +62,47 @@ function GetUsers() {
                 key={index}
                 className='data-item' 
             >
+                
+                <div 
+                    className='test-title'
+                    onClick={() => toggle(index)}
+                >
+                    <h2 className='test-heading'>{course}</h2>
+                    <img 
+                        src={Chevron} 
+                        alt='chevron icon'
+                        className={selected === index ? 'chevron-open' : 'chevron-close'}
+                    />
+                </div>
 
-                <table style={{width: "100%"}}>
+                <table 
+                    className={selected === index ? 'table-open' : 'table-close'}
+                    // style={{height: `${selected ? '100%' : '0px'}`}}
+                >
+
+                    {/* <th className='data-item-heading'>{course}</th> */}
+
+
+
                     <thead>
-                        <tr>
-                            <th className='data-item-heading'>{course}</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
                         <tr>
                             <td className='data-item-titles'>LA</td>
                             <td className='data-item-titles'>Asset</td>
                             <td className='data-item-titles'>WPA</td>
-                            <td className='data-item-titles'>Check-out</td>
+                            {/* <td className='data-item-titles'>Check-out</td> */}
                         </tr>
+                    </thead>
+
+                    <tbody> 
 
                         {globalState.users && globalState.users.sort((a, b) => /^[0-9]/.test(a.la) - /^[0-9]/.test(b.la) || a.la.localeCompare(b.la, undefined, { numeric: true })).map((data, index) => {
                             return (
                                 course === data.course && 
-                                <>
-                                    <tr 
-                                        key={index}
+                                    <>
+                                
+                                    <tr                                         
                                         className='data-item-contents'
+                                        key={index}
                                     >
                                         <td>{data.la}</td>
                                         <td>{data.asset ? data.asset : "N/A"}</td>
@@ -95,7 +124,8 @@ function GetUsers() {
                                             <hr className='data-item-line' />
                                         </td>
                                     </tr>
-                                </>                           
+
+                                </>
                             )
                         })}
                     </tbody>
